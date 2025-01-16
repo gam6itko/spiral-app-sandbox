b3cff619fe6204d595283a497227534743693ac5
Alexander Strizhak <gam6itko@gmail.com>
2025-01-16 14:19:57 +0300

    up

--- src/app/app/src/Command/SendMailCommand.php
+++ src/app/app/src/Command/SendMailCommand.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use Spiral\Console\Command;
 use Spiral\Console\Attribute as Console;
+use Spiral\Console\Command;
 use Spiral\Mailer\MailerInterface;
 use Spiral\Mailer\Message;
-use Symfony\Component\Console\Output\OutputInterface;
 
 #[Console\AsCommand(
     name: 'app:send-mail',
@@ -17,6 +16,15 @@ final class SendMailCommand extends Command
 {
     protected function perform(MailerInterface $mailer): void
     {
-        $mailer->send(new Message('hello.dark.php', 'trap@buggregator.com'));
+        $mailer->send(
+            new Message(
+                subject: 'hello.dark.php',
+                to: [
+                    'User1 <gam6itko@mail.ru>',
+                    'User2 <gam6itko@gmail.com>',
+                    'User3 <gam6itko@yandex.ru>',
+                ],
+            ),
+        );
     }
 }
